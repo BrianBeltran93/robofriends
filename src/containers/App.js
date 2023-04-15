14fca1e81d9e7da92efb771f111e49cf2c5d0c3f
Brian Beltran <brianbeltran93@gmail.com>
2023-04-15 15:59:42 -0400

    revert hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
+<<<<<<< HEAD
 function App() {
     const [robots, setRobots] = useState([])
     const [searchfield, setSearchField] = useState('')
@@ -36,6 +37,47 @@ function App() {
                 </Scroll>
             </div>
         );
+=======
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render() {
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        })
+        if (robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundary>
+                    </Scroll>
+                </div>
+            );
+        }
+>>>>>>> parent of 58c87005 (Converted App class to use hooks instead)
     }
 }
 
